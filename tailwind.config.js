1669dec0fb8d0a86c26ab871d2549fac5304be9f
Leandro <leandroquiroga9514@gmail.com>
2021-10-24 00:20:21 -0300

    Add themes extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,27 @@ module.exports = {
   purge: [
     './src/**/*.html'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       fontFamily: {
         open: ['Open Sans', 'sans-serif']
       },
       width: {
-        '120': '38rem'
+        '120': '38rem',
+        '87' : '87%'
       },
       maxHeight: {
-        '3/4': '90vh' 
-      }
+        'content': '90vh',
+      },
+      gridTemplateRows: {
+       'layout': 'repeat(auto-fit, minmax(250px, auto))',
+      },
+      gridTemplateColumns: {
+        'layout-1': 'repeat(4, minmax(275px, auto))',
+        'layout-2': 'repeat(2, minmax(400px, auto))',
+        'layout-3': 'repeat(1, minmax(75%, auto))',
+       }
     },
   },
   variants: {
